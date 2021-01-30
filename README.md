7a1fa5bf06afb27c9fd2707c5b34a786d281d3ab
gapiyka <50524296+p-pelmeni@users.noreply.github.com>
2021-01-30 20:13:24 +0200

    change README

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# HLTV Bot
- 
+## HLTVBot
+[Бот в Телеграм](https://t.me/HltvHelpBot)
+
+## Для чего бот?
+Этот бот предназначен для взаимодействия с [потралом HLTV](https://hltv.org/) через "удобный" интерфейс при помощи Телеграма
+
+## API/Фрэймворки
+- [Telegraf](https://www.npmjs.com/package/telegraf)
+- [HLTV-API](https://www.npmjs.com/package/hltv)
+
+⚠️ WARNING: Abusing this library will likely result in an IP ban from HLTV simply 
+because ofCloudflare bot protection. Please use with caution and try to limit the 
+rate and amount of your requests if you value your access to HLTV.
+
+## Как использовать?
+📕Для работы с ботом перейдите по [ссылке](https://t.me/HltvHelpBot)
+📙После начала работы с ботом, в нижней части экрана будут доступны 4 спецификации
+📘| Matches - для просмотра матчей | Events - для просмотра турниров |
+📗| Players - для взаимодействия с игроками | Teams - для взаимодействия с командами |
+
+## Лицензия
+[MIT License](https://github.com/gapiyka/HLTV-Bot/blob/main/LICENSE)
--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const { Telegraf, Markup } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);//BOT_TOKEN in .env file
 
 const BUTTON_REQUESTS = {
-    MATCHES: ['Live matches', 'Today+Tommorow', 'List of 15 matches', 'Intresting matches'],
+    MATCHES: ['Live matches', 'List of 15 matches', 'Intresting matches', 'Results'],
     EVENTS: ['This year events', 'Large tournaments', 'Search by name'],
     PLAYERS: ['Top 10 players', 'Search by name'],
-    TEAMS: ['Top 5 teams', "Search team"]
-}
+    TEAMS: ['Top 5 teams', 'Search team']
+};
+
+const MONTH = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const _MONTH = MONTH.map(x => x.slice(0, 3));
 
 //FUNC::
 
 const matchfunc = async (ctx) => {
     let data = "Choose which variation of list you want receive:";
-    let matches = await HLTV.getMatches();
     let inline_keyboard = [];
 
     BUTTON_REQUESTS.MATCHES.forEach(element => inline_keyboard.push([
@@ -36,14 +38,61 @@ const matchfunc = async (ctx) => {
 };
 
 const eventsfunc = async (ctx) => {
-    let data = 'Here is a list of upcoming tournaments:\n';
-    let events = await HLTV.getEvents();
+    let data = "Choose one:";
+    let inline_keyboard = [];
 
-    //TODO
+    BUTTON_REQUESTS.EVENTS.forEach(element => inline_keyboard.push([
+        {
+            text: element,
+            callback_data: element.toString(),
+        },
+    ]))
+    const keyboard = {
+        reply_markup: JSON.stringify({
+            inline_keyboard,
+        }),
+    };
 
-    ctx.reply(data);
+    ctx.reply(data, keyboard);
 };
 
+const playersfunc = async (ctx) => {
+    let data = "Choose one:";
+    let inline_keyboard = [];
+
+    BUTTON_REQUESTS.PLAYERS.forEach(element => inline_keyboard.push([
+        {
+            text: element,
+            callback_data: element.toString(),
+        },
+    ]))
+    const keyboard = {
+        reply_markup: JSON.stringify({
+            inline_keyboard,
+        }),
+    };
+
+    ctx.reply(data, keyboard);
+};
+
+const teamsfunc = async (ctx) => {
+    let data = "Choose one:";
+    let inline_keyboard = [];
+
+    BUTTON_REQUESTS.TEAMS.forEach(element => inline_keyboard.push([
+        {
+            text: element,
+            callback_data: element.toString(),
+        },
+    ]))
+    const keyboard = {
+        reply_markup: JSON.stringify({
+            inline_keyboard,
+        }),
+    };
+
+    ctx.reply(data, keyboard);
+};
 
 const queryForLive = async (ctx) => {
     let data = 'Here is a list of live:';
@@ -66,7 +115,7 @@ const queryForList15 = async (ctx) => {
         if (matches[i].live) data += '\nLive match: ';
         else {
             let date = new Date(matches[i].date);
-            data += `\n${date.getFullYear()}-${date.getDate()}-${date.getMonth()} [${date.getHours()} : ${date.getMinutes()}] `;
+            data += `\n${date.getDate()}-${_MONTH[date.getMonth()]} [${date.getHours()} : ${date.getMinutes()}] `;
         }
         try {
             data += `🎮${matches[i].team1.name} vs ${matches[i].team2.name}🎮`;
@@ -79,12 +128,102 @@ const queryForList15 = async (ctx) => {
     ctx.reply(data);
 }
 
-const queryForDays = async (ctx) => {
+const queryForStars = async (ctx) => {
+    let data = 'Here is a list of matches:';
+    let matches = await HLTV.getMatches();
 
+    matches.forEach(x => {
+        if (x.stars > 0 && x.team1 != undefined) {
+            if (x.live) data += '\nLive match: ';
+            else {
+                let date = new Date(x.date);
+                data += `\n${date.getDate()}-${_MONTH[date.getMonth()]} [${date.getHours()} : ${date.getMinutes()}] `;
+            }
+            data += `\n🎮${x.team1.name} vs ${x.team2.name}🎮\n`;
+        }
+    })
+    if (data == 'Here is a list of matches:') ctx.reply('☕️Drink cup of coffee, nothing intresting.🧩');
+    else ctx.reply(data);
 }
 
-const queryForStars = async (ctx) => {
+const queryForResults = async (ctx) => {
+    let data = 'Here is a list of matches:';
+    let results = await HLTV.getResults({ startPage: 0, endPage: 1 });
 
+    results.forEach(x => {
+        if (x.stars > 0) {
+            data += `\n🎮${x.team1.name} vs ${x.team2.name} 🎮 RESULT: ${x.result} 🎮\n`;
+        }
+    });
+    ctx.reply(data);
+}
+
+const queryForYearEvents = async (ctx) => {
+    let data = '🏆T O U R N A M E N T S:🏆\n\n';
+    let events = await HLTV.getEvents();
+
+    events.forEach(kvartal => {
+        data += `\n${MONTH[kvartal.month]}:\n`
+        const eventsArr = kvartal.events;
+        eventsArr.forEach(event => {
+            let dateS = new Date(event.dateStart);
+            let dateE = new Date(event.dateEnd);
+            data += `🏆${event.name}🏆 | ${_MONTH[dateS.getMonth()]} ${dateS.getDate()} - ${_MONTH[dateE.getMonth()]} ${dateE.getDate()}\n`;
+        });
+        data += '\n';
+    })
+
+    ctx.reply(data);
+}
+
+const queryForTopEvents = async (ctx) => {
+    let data = '🏆T O U R N A M E N T S:🏆\n\n';
+    let events = await HLTV.getEvents();
+
+    events.forEach(kvartal => {
+        data += `\n${MONTH[kvartal.month].toUpperCase()}:\n`
+        const eventsArr = kvartal.events;
+        eventsArr.forEach(event => {
+            if (event.prizePool != 'Other' && event.prizePool.length >= 8) {
+                let dateS = new Date(event.dateStart);
+                let dateE = new Date(event.dateEnd);
+                data += `🏆${event.name}🏆\n${_MONTH[dateS.getMonth()]} ${dateS.getDate()} - ${_MONTH[dateE.getMonth()]} ${dateE.getDate()} | Prize: ${event.prizePool}💰\n\n`;
+            }
+        });
+        data += '\n';
+    })
+
+    ctx.reply(data);
+}
+
+const queryForEvent = async (ctx) => {
+    let data = 'Here is a:';
+
+    ctx.reply(data);
+}
+
+const queryForTopPlayers = async (ctx) => {
+    let data = 'Here is a:';
+
+    ctx.reply(data);
+}
+
+const queryForPlayer = async (ctx) => {
+    let data = 'Here is a:';
+
+    ctx.reply(data);
+}
+
+const queryForTopTeams = async (ctx) => {
+    let data = 'Here is a:';
+
+    ctx.reply(data);
+}
+
+const queryForTeam = async (ctx) => {
+    let data = 'Here is a:';
+
+    ctx.reply(data);
 }
 
 const onCallbackQuery = (ctx) => {
@@ -92,10 +231,20 @@ const onCallbackQuery = (ctx) => {
     switch (data) {
         case 'Live matches': queryForLive(ctx); break;
         case 'List of 15 matches': queryForList15(ctx); break;
-        case 'Today+Tommorow': queryForDays(ctx); break;
         case 'Intresting matches': queryForStars(ctx); break;
+        case 'Results': queryForResults(ctx); break;
+        case 'This year events': queryForYearEvents(ctx); break;
+        case 'Large tournaments': queryForTopEvents(ctx); break;
+        case 'Search event': queryForEvent(ctx); break;
+        case 'Top 10 players': queryForTopPlayers(ctx); break;
+        case 'Search by name': queryForPlayer(ctx); break;
+        case 'Top 5 teams': queryForTopTeams(ctx); break;
+        case 'Search team': queryForTeam(ctx); break;
     };
 };
+
+
+
 //DEFAULT
 bot.start((ctx) => {
     ctx.reply('Welcome dude',
@@ -110,9 +259,9 @@ bot.help((ctx) => ctx.reply('🖐Use buttons at bottom 👇 of screen to find in
 
 //HEARS + COMMANDS
 bot.hears('Matches', matchfunc);
-
-
 bot.hears('Events', eventsfunc);
+bot.hears('Players', playersfunc);
+bot.hears('Teams', teamsfunc);
 
 
 bot.on('callback_query', ctx => {
